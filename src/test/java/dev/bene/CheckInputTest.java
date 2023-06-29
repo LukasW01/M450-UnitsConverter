bf051d34f898a3d20f979fbd291b61fb1cda1fa0
LukasW01 <lukas@wigger.one>
2023-06-30 01:32:07 +0200

    removed testcaeses

--- src/test/java/dev/bene/CheckInputTest.java
+++ src/test/java/dev/bene/CheckInputTest.java
@@ -1,7 +1,6 @@
 package dev.bene;
 
 import javax.swing.text.*;
-import org.mockito.Mockito;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,45 +10,6 @@ public class CheckInputTest {
     private Document document;
     private AttributeSet attr;
     private StringBuilder sb;
-    private DocumentFilter.FilterBypass mockFilterBypass;
-
-    public CheckInputTest() {
-        checkInput = new CheckInput();
-        document = new PlainDocument();
-        attr = new SimpleAttributeSet();
-        sb = new StringBuilder();
-
-        mockFilterBypass = Mockito.mock(DocumentFilter.FilterBypass.class);
-    }
-
-    @Test
-    public void testInsertReplace() {
-        try {
-            // numeric
-            sb.append("123");
-            document.insertString(0, sb.toString(), attr);
-
-            Mockito.when(mockFilterBypass.getDocument()).thenReturn(document);
-            checkInput.insertString(mockFilterBypass, 1, "4", attr);
-            checkInput.replace(mockFilterBypass, 1, 1, "a", attr);
-
-            assertEquals("123", document.getText(0, document.getLength()));
-
-            // string
-            sb = new StringBuilder();
-            sb.append("abc");
-            document.remove(0, document.getLength());
-            document.insertString(0, sb.toString(), attr);
-
-            Mockito.when(mockFilterBypass.getDocument()).thenReturn(document);
-            checkInput.insertString(mockFilterBypass, 1, "4", attr);
-            checkInput.replace(mockFilterBypass, 1, 1, "d", attr);
-
-            assertEquals("abc", document.getText(0, document.getLength()));
-        } catch (BadLocationException e) {
-            System.err.println("BadLocationException" + e);
-        }
-    }
 
     @Test
     public void testIsNumeric() {
