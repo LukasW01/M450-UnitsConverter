f342cc806e3dea4b02e38eb0474b9ea977894438
LukasW01 <lukas@wigger.one>
2023-12-31 14:53:16 +0100

    remove deleted symbol in dbtest

--- src/test/java/dev/bene/DatabaseTest.java
+++ src/test/java/dev/bene/DatabaseTest.java
@@ -1,13 +1,11 @@
 package dev.bene;
 
-import dev.bene.db.MongoDB;
 import dev.bene.db.SQLite;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DatabaseTest {
 
-    //private MongoDB mongoDB;
     private SQLite sqlite;
 
 
@@ -15,15 +13,6 @@ public class DatabaseTest {
         //mongoDB = new MongoDB();
         sqlite = new SQLite();
     }
-
-
-    /*
-    @Test
-    public void testDBOperations() {
-        mongoDB.setHistory(new Converter(1, 1000, "KILOMETER", "METERS", "input * 1000.0", "KILOMETERS").toBSON());
-        assertNotNull(mongoDB.getHistory());
-    }
-     */
 
     @Test
     public void testDBOperations() {
